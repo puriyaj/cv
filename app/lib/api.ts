cb4fea208b8a01502803853b6e58d30e0c1fe3c4
pouria <puriyaj@gmail.com>
2024-01-21 23:38:21 +0100

    api

--- app/lib/api.ts
+++
@@ -1,8 +0,0 @@
-export const sendContact = async (data) => fetch('/api/contact',{
-  method: "POST",
-  body: JSON.stringify(data),
-  headers:{
-    "content-type":"application/json",
-    Accept: "application/json",
-  }
-})
