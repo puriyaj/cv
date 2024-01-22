a9111e7e41a826ca8097e10fa5e1ba25129ef989
pouria <puriyaj@gmail.com>
2024-01-22 09:42:34 +0100

    s

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -8,7 +8,7 @@ const Project = () => {
     <div className="flex flex-col w-80 rounded-lg  p-3 shadow-2xl bg-slate-100 dark:bg-slate-700">
      <Image src="/images/carpet.jpg" width={300} height={300} alt="carpet website" className="rounded-lg "/>
      <h1 className="text-xl p-3">Eleishe</h1>
-     <p className="text-gray-300 ">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
+     <p className="dark:text-gray-300 text-gray-700">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
      <ul className="flex flex-row gap-2 flex-wrap pt-3 pb-3">
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">Front-end</li>
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">Laravel</li>
@@ -25,7 +25,7 @@ const Project = () => {
   <div className="flex flex-col w-80 rounded-lg bg-slate-100 dark:bg-slate-700 p-3">
      <Image src="/images/indie-game.jpg" width={300} height={300} alt="carpet website" className="rounded-lg "/>
      <h1 className="text-xl p-3">Game Reviews</h1>
-     <p className="text-gray-300 ">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
+     <p className="dark:text-gray-300 text-gray-700 ">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
      <ul className="flex flex-row gap-2 flex-wrap pt-3 pb-3">
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">Full Stack</li>
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">Next.Js</li>
@@ -39,7 +39,7 @@ const Project = () => {
   <div className="flex flex-col w-80 rounded-lg bg-slate-100 dark:bg-slate-700 p-3">
      <Image src="/images/vet-one.jpg" width={300} height={300} alt="carpet website" className="rounded-lg "/>
      <h1 className="text-xl p-3">Vet-one</h1>
-     <p className="text-gray-300 ">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
+     <p className="dark:text-gray-300 text-gray-700 ">Eleishe is a website that sells carpets. I worked on both the design and functionality of the site, handling both the front and back ends. I also helped with making sure user accounts are secure. This project was my last one at Tuv International.</p>
      <ul className="flex flex-row gap-2 flex-wrap pt-3 pb-3">
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">Full Stack</li>
       <li className="whitespace-nowrap rounded-full bg-gray-600 dark:bg-gray-50 px-2.5 py-0.5 text-xs text-gray-100 dark:text-gray-900">SQL</li>
